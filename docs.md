6c33a4f6d4cf35370720527e9806a31cab0445d9
Daniel Brommer <daniel.brommer@student.kit.edu>
2025-06-02 16:21:11 +0200

    Add tests for gpu to host communication

--- docs.md
+++ docs.md
@@ -3,7 +3,9 @@
 ## Supported Send/Recv Buffers 
 | Data Type   | Supported as send/recv buffer? | Notes                |
 |-------------|:------------------:|----------------------|
-| `std:span`       | ✅                 |                      |
+| `std::span`       | ✅                 |                      |
+| `std::span from device memory to host memory`       | ✅                 |                      |
+| `std::span from device memory to host std::vector`       | ✅                 |                      |
 | `raw gpu pointer`     | ❌                 |static assertion failed: MPI does not support pointer types. Why do you want to transfer a pointer over MPI?|     
 | `raw gpu pointer with size and type`     | ❌                 |MPI is called but result is wrong: DataBuffer::value_type_with_const* data() calls return &underlying() which won't return the original gpu pointer -> MPI is called with wrong pointer|                           
 | `thrust::host_vector` | ✅                 |                      |
--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@
 #define N 10
 #define X 42
 
+#define RUN_TEST(test) \
+    test(rank);        \
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
 void print_and_test(const char* test_name, const float* h_result) {
     std::cout << test_name << " Rank 1 received: ";
     for (int i = 0; i < N; ++i) std::cout << h_result[i] << " ";
@@ -23,6 +28,7 @@ void print_and_test(const char* test_name, const float* h_result) {
     }
 }
 
+// MPI with raw gpu pointers
 void mpi_test_raw_pointer(int rank) {
     float* d_buf;
     cudaMalloc((void**)&d_buf, N * sizeof(float));
@@ -32,6 +38,7 @@ void mpi_test_raw_pointer(int rank) {
         for (int i = 0; i < N; ++i) h_data[i] = X;
         cudaMemcpy(d_buf, h_data, N * sizeof(float), cudaMemcpyHostToDevice);
 		MPI_Send(d_buf, N, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+
     } else if (rank == 1) {
         MPI_Recv(d_buf, N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         float h_result[N];
@@ -41,6 +48,7 @@ void mpi_test_raw_pointer(int rank) {
     cudaFree(d_buf);
 }
 
+// KaMPIng with gpu pointers in std::span
 void kamping_test_span(int rank) {
     kamping::Communicator comm;
     float* d_buf;
@@ -55,7 +63,6 @@ void kamping_test_span(int rank) {
 
     } else if (rank == 1) {
         comm.recv(kamping::recv_buf<kamping::no_resize>(d_span));
-
         float h_result[N];
         cudaMemcpy(h_result, d_buf, N * sizeof(float), cudaMemcpyDeviceToHost);
         print_and_test("Kamping span", h_result);
@@ -63,8 +70,10 @@ void kamping_test_span(int rank) {
     cudaFree(d_buf);
 }
 
-void kamping_test_thrust_device(int rank) {
+// KaMPIng with thrust host vector
+void kamping_test_thrust_host_vec(int rank) {
     kamping::Communicator comm;
+
     if (rank == 0) {
         thrust::host_vector<float> h_vec(N, X);
         comm.send(kamping::send_buf(h_vec), kamping::destination(1));
@@ -72,27 +81,26 @@ void kamping_test_thrust_device(int rank) {
     } else if (rank == 1) {
         thrust::host_vector<float> h_vec(N, 99);
         comm.recv(kamping::recv_buf<kamping::no_resize>(h_vec));
-
-        //thrust::host_vector<int> h_vec(d_vec.begin(), d_vec.end());
         print_and_test("Kamping thrust host vec", h_vec.data());
     }
 }
  
-void mpi_test_thrust_device(int rank) {
+// MPI with thrust host vector
+void mpi_test_thrust_host_vec(int rank) {
     if (rank == 0) {
-        thrust::host_vector<float> d_vec(N, X);
-        MPI_Send(d_vec.data(), N, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+        thrust::host_vector<float> h_vec(N, X);
+        MPI_Send(h_vec.data(), N, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+
     } else if (rank == 1) {
-        thrust::host_vector<float> d_vec(N, 99);
-        MPI_Recv(d_vec.data(), N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        thrust::host_vector<float> h_vec(d_vec.begin(), d_vec.end());
+        thrust::host_vector<float> h_vec(N, 99);
+        MPI_Recv(h_vec.data(), N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         print_and_test("MPI thrust host vec", h_vec.data());
     }
 }
 
+// KaMPIng with raw gpu pointers with send/recv count and type
 void kamping_test_raw_pointer(int rank) {
     kamping::Communicator comm;
-
     float* d_buf;
     cudaMalloc((void**)&d_buf, N * sizeof(float));
 
@@ -101,6 +109,7 @@ void kamping_test_raw_pointer(int rank) {
         for (int i = 0; i < N; ++i) h_data[i] = X;
         cudaMemcpy(d_buf, h_data, N * sizeof(float), cudaMemcpyHostToDevice);
         comm.send(kamping::send_buf(d_buf), kamping::destination(1), kamping::send_count(N), kamping::send_type(MPI_FLOAT));
+
     } else if (rank == 1) {
         comm.recv(kamping::recv_buf<kamping::no_resize>(d_buf), kamping::recv_count(N), kamping::recv_type(MPI_FLOAT));
         float h_result[N];
@@ -110,26 +119,85 @@ void kamping_test_raw_pointer(int rank) {
     cudaFree(d_buf);
 }
 
+// MPI with raw gpu pointer to raw host pointer
+void mpi_test_raw_pointer_to_host(int rank) {
+    if (rank == 0) {
+        float* d_buf;
+        cudaMalloc((void**)&d_buf, N * sizeof(float));
+        float h_data[N];
+        for (int i = 0; i < N; ++i) h_data[i] = X;
+        cudaMemcpy(d_buf, h_data, N * sizeof(float), cudaMemcpyHostToDevice);
+		MPI_Send(d_buf, N, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+        cudaFree(d_buf);
+
+    } else if (rank == 1) {
+        float* h_buf = (float*) malloc(N * sizeof(float));
+        for (int i = 0; i < N; ++i) h_buf[i] = 99; 
+        MPI_Recv(h_buf, N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        print_and_test("MPI raw gpu pointer to host", h_buf);
+        free(h_buf);
+    }   
+}
+
+// KaMPIng with gpu span to host span
+void kamping_test_span_to_host(int rank) {
+    kamping::Communicator comm;
+
+    if (rank == 0) {
+        float* d_buf;
+        cudaMalloc((void**)&d_buf, N * sizeof(float));
+        std::span<float> d_span(d_buf, N);
+        float h_data[N];
+        for (int i = 0; i < N; ++i) h_data[i] = X;
+        cudaMemcpy(d_buf, h_data, N * sizeof(float), cudaMemcpyHostToDevice);
+        comm.send(kamping::send_buf(d_span), kamping::destination(1));
+        cudaFree(d_buf);
+
+    } else if (rank == 1) {
+        float* h_buf = (float*) malloc(N * sizeof(float));
+        for (int i = 0; i < N; ++i) h_buf[i] = 99;
+        std::span<float> d_span(h_buf, N);
+        comm.recv(kamping::recv_buf<kamping::no_resize>(d_span));
+        print_and_test("Kamping gpu span to host", d_span.data());
+        free(h_buf);
+    }    
+}
+
+// KaMPIng with gpu span to host vector
+void kamping_test_span_to_host_vec(int rank) {
+    kamping::Communicator comm;
+
+    if (rank == 0) {
+        float* d_buf;
+        cudaMalloc((void**)&d_buf, N * sizeof(float));
+        std::span<float> d_span(d_buf, N);
+        float h_data[N];
+        for (int i = 0; i < N; ++i) h_data[i] = X;
+        cudaMemcpy(d_buf, h_data, N * sizeof(float), cudaMemcpyHostToDevice);
+        comm.send(kamping::send_buf(d_span), kamping::destination(1));
+        cudaFree(d_buf);
+
+    } else if (rank == 1) {
+        std::vector<float> h_buf(N, 99);
+        comm.recv(kamping::recv_buf<kamping::no_resize>(h_buf));
+        print_and_test("Kamping gpu span to host vector", h_buf.data());
+    }    
+}
+
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    mpi_test_raw_pointer(rank);
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    kamping_test_span(rank);
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    kamping_test_thrust_device(rank);
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    mpi_test_thrust_device(rank);
-    MPI_Barrier(MPI_COMM_WORLD);
-    
-    kamping_test_raw_pointer(rank);
-    MPI_Barrier(MPI_COMM_WORLD);
+    RUN_TEST(mpi_test_raw_pointer);
+    RUN_TEST(kamping_test_span);
+    RUN_TEST(kamping_test_thrust_host_vec);
+    RUN_TEST(mpi_test_thrust_host_vec)
+    // RUN_TEST(kamping_test_raw_pointer);
+    RUN_TEST(mpi_test_raw_pointer_to_host);
+    RUN_TEST(kamping_test_span_to_host)
+    RUN_TEST(kamping_test_span_to_host_vec)
 
     MPI_Finalize();
     return 0;   
